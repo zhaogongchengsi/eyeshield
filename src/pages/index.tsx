c0f2a927dc98a303f36cb5fbf0eee71fa604cbac
汪克兴 <wangkexing@192.168.10.4>
2024-03-12 19:47:19 +0800

    添加暗黑模式

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import { Button } from "@radix-ui/themes";
 import { useTheme } from "~/hooks/theme";
 import { Button as SemiButton } from "@douyinfe/semi-ui";
+import Header from "~/components/header";
 
 export default function Index() {
   const [_, setTheme] = useTheme();
 
   return (
     <div className="p-2">
-      <h3>Welcome Home!</h3>
+      <Header user="wkx"/>
       <Button onClick={() => setTheme("dark")}>dark</Button>
       <SemiButton onClick={() => setTheme("light")}>light</SemiButton>
     </div>
