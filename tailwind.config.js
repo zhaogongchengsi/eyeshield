f59183418b748137cb7b2a76ade9631b61af4de3
zhaogongchengsi <zzh1586169624@163.com>
2024-03-12 20:00:40 +0800

    feat: add darkMode class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
 import { radixThemePreset } from 'radix-themes-tw';
 
-// eslint-disable-next-line no-undef
 module.exports = {
 	darkMode:'class',
 	presets: [radixThemePreset],
-	theme: {
-        extend: {
-            colors: {
-                dark:'#121212'
-            },
-        },
-    },
 }
